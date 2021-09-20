edb72b8210023470052e9ff55d9600d7bd357de2
Almir2021 <bosnjicalmir@gmail.com>
2021-09-21 00:31:52 +0200

    vjezba broj 10 ponovo

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -5,13 +5,18 @@
             //Only change code below this line
             return undefined;
         } // Only change code above this line
+
+        else if (x = 0) 
+        return 0;
+
+        
             
         return Math.round(Math.pow(Math.sqrt(x) + Math.sqrt(y),2));
      } 
       
      
      //Only change code below this line
-     console.log( mathTest (2,8));
+     console.log( mathTest (0,0));
  // Only change code above this line
 
  module.exports = mathTest;
