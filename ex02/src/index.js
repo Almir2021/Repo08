d5b0901064c137a6d9e70001ecb20b8d47da78c5
Almir2021 <bosnjicalmir@gmail.com>
2021-09-20 13:41:53 +0200

    Vjezba broj 2 parametri

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function functionPrintInfo( name, age){
+
+    // Write code below this line 
+    
+    console.log(name,age);
+
+    // Write code above this line 
+    
+ } functionPrintInfo("john",23 );
+
+ module.exports = functionPrintInfo;
