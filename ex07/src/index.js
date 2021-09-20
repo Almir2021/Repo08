2672e839ad13b2e3cec96525720e86fbb7f854ea
Almir2021 <bosnjicalmir@gmail.com>
2021-09-20 21:02:24 +0200

    vjezba 7

---
+++ ex07/src/index.js
@@ -0,0 +1,19 @@
+//Only change code below this line
+var sum = 0;
+
+function addThree() {
+    sum += 3;
+
+   return sum;    
+
+}console.log( addThree());
+
+function addFive() {
+   
+    sum += 5;
+    return sum;
+
+ }
+ console.log(addFive());
+ // Only change code above this line
+    
