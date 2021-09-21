20a31444c9361ffa4d0614fc1882cca32127796e
Almir2021 <bosnjicalmir@gmail.com>
2021-09-21 06:25:44 +0200

    Vjezba random 0 29

---
+++ ex12/src/index.js
@@ -0,0 +1,10 @@
+
+//Only change code below this line
+function randomInteger () {
+    var random1 = Math.floor(Math.random() * 30);
+    
+     return random1;
+    
+} console.log( randomInteger());
+// Only change code above this line
+module.exports = randomInteger;
