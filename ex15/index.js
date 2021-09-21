c968ad4c8b7f581090ce83b173a4bdcffa3f8990
Almir2021 <bosnjicalmir@gmail.com>
2021-09-21 07:28:23 +0200

    Vjezba 15

---
+++ ex15/index.js
@@ -0,0 +1,9 @@
+//Only change code below this line
+function convertToInt (str) {
+    var convert = parseInt(str,16)
+    return convert;
+
+
+} console.log (convertToInt("JeffBezos"));
+// Only change code above this line
+module.exports = convertToInt ;
