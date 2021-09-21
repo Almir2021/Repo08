63bd25d75c79db4dd051a9374ac0b63b823f219d
Almir2021 <bosnjicalmir@gmail.com>
2021-09-21 06:56:08 +0200

    Dodajem vjezbu 14 prasa

---
+++ ex14/src/index.js
@@ -0,0 +1,10 @@
+
+//Only change code below this line
+function convertStrToInt (str){
+  var strToint = parseInt(str)
+  return strToint;
+
+
+} console.log( convertStrToInt("JeffBezos"));
+// Only change code above this line
+module.exports = convertStrToInt ;
